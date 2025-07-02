00ddfb87630cc98e03dc5e506ce01bc257fa6e06
oliveroliverio <oliveroliverio@gmail.com>
2025-07-01 17:58:00 -0700

    added form validation

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -24,11 +24,22 @@ export const LoginView = () => {
         <form onSubmit={handleSubmit}>
             <label>
                 Username:
-                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    minLength="3"
+                />
             </label>
             <label>
                 Password:
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
             </label>
             <button type="submit">Submit</button>
         </form>
